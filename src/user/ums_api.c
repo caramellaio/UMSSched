f076e193549bf2ecbb3e8e32642acd1379d31fe5
abombardelli <abombardelli@fbk.eu>
2021-08-08 18:48:27 +0200

    Include pthread in EnterUMSScheduling
    Compile the user mode code with `pthread_create`.

--- src/user/ums_api.c
+++ src/user/ums_api.c
@@ -1,11 +1,25 @@
 #include "ums_api.h"
 #include <stddef.h>
+#include <pthread.h>
+#include <stdio.h>
 
+
+static void* do_gen_ums_sched(void *args);
 
 int EnterUmsSchedulingMode(struct ums_entry_point entry_point,
                            struct ums_completion_list comp_list)
 {
-  return 0;
+  pthread_t ums_pthread;
+
+  int err = 0;
+
+  err = pthread_create(&ums_pthread, NULL, &do_gen_ums_sched, NULL);
+
+  if (err) {
+    fprintf(stderr, "Error: cannot create User Mode Scheduler thread!\n");
+  }
+
+  return err;
 }
 
 void ExecuteUmsThread(struct ums_scheduler* scheduler,
@@ -24,4 +38,11 @@ void UmSThreadYield(struct ums_scheduler* scheduler,
 struct ums_worker* DequeueUmsCompletionListItems(struct ums_scheduler* scheduler)
 {
   return NULL;
+}
+
+static void* do_gen_ums_sched(void *args)
+{
+  printf("Executing function: %s\n", __func__);
+
+  return 0;
 }
