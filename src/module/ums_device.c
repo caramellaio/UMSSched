c352429ed95d164eeede0e3fbbf79e2e1f7d0a9d
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-14 11:05:26 +0200

    Fix ums device linux kernel indentation

--- src/module/ums_device.c
+++ src/module/ums_device.c
@@ -29,33 +29,32 @@ static long device_ioctl(struct file *file, unsigned int request, unsigned long 
  * Global variables are declared as static, so are global within the file.
  */
 static struct file_operations fops = {
-    .unlocked_ioctl = device_ioctl};
+	.unlocked_ioctl = device_ioctl};
 
 static struct miscdevice mdev = {
-    .minor = 0,
-    .name = DEVICE_NAME,
-    .mode = S_IALLUGO,
-    .fops = &fops};
+	.minor = 0,
+	.name = DEVICE_NAME,
+	.mode = S_IALLUGO,
+	.fops = &fops};
 
 /*
  * This function is called when the module is loaded
  */
 int init_module(void)
 {
-    int ret;
-    printk(KERN_DEBUG MODULE_NAME_LOG "init\n");
+	int ret;
+	printk(KERN_DEBUG MODULE_NAME_LOG "init\n");
 
-    ret = misc_register(&mdev);
+	ret = misc_register(&mdev);
 
-    if (ret < 0)
-    {
-        printk(KERN_ALERT MODULE_NAME_LOG "Registering char device failed\n");
-        return ret;
-    }
+	if (ret < 0) {
+		printk(KERN_ALERT MODULE_NAME_LOG "Registering char device failed\n");
+		return ret;
+	}
 
-    printk(KERN_DEBUG MODULE_NAME_LOG "Device registered successfully\n");
+	printk(KERN_DEBUG MODULE_NAME_LOG "Device registered successfully\n");
 
-    return SUCCESS;
+	return SUCCESS;
 }
 
 /*
@@ -63,46 +62,48 @@ int init_module(void)
  */
 void cleanup_module(void)
 {
-    /*
-     * Unregister the device
-     */
-    misc_deregister(&mdev);
+	/*
+	* Unregister the device
+	*/
+	misc_deregister(&mdev);
 
-    printk(KERN_DEBUG MODULE_NAME_LOG "exit\n");
+	printk(KERN_DEBUG MODULE_NAME_LOG "exit\n");
 }
 
 static long device_ioctl(struct file *file, unsigned int request, unsigned long data)
 {
-    printk(KERN_DEBUG MODULE_NAME_LOG "device_ioctl: pid->%d, path=%s, request=%u\n", current->pid, file->f_path.dentry->d_iname, request);
+	printk(KERN_DEBUG MODULE_NAME_LOG
+	       "device_ioctl: pid->%d, path=%s, request=%u\n", current->pid,
+	       file->f_path.dentry->d_iname, request);
 
-    switch (request) {
-      case UMS_REQUEST_ENTER_UMS_SCHEDULING:
-        {
+	switch (request) {
+	case UMS_REQUEST_ENTER_UMS_SCHEDULING:
+	{
 
-        }
-        break;
+	}
+	break;
 
-      case UMS_REQUEST_REGISTER_SCHEDULER_THREAD:
-        {
+	case UMS_REQUEST_REGISTER_SCHEDULER_THREAD:
+	{
 
-        }
-        break;
+	}
+	break;
 
-      case UMS_REQUEST_NEW_COMPLETION_LIST:
-        {
+	case UMS_REQUEST_NEW_COMPLETION_LIST:
+	{
 
-        }
-        break;
+	}
+	break;
 
-      case UMS_REQUEST_REGISTER_COMPLETION_ELEM:
-        {
+	case UMS_REQUEST_REGISTER_COMPLETION_ELEM:
+	{
 
-        }
-        break;
-      default: return FAILURE;
-    }
+	}
+	break;
+	default: return FAILURE;
+	}
 
-    return SUCCESS;
+	return SUCCESS;
 }
 
 MODULE_LICENSE("GPL");
