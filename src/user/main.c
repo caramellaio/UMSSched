2239f1192ce25475004c99c39451fa8872e433c9
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-16 10:14:34 +0200

    Test actual state of the device with ioctl call

--- src/user/main.c
+++ src/user/main.c
@@ -1,5 +1,16 @@
 #include "ums_api.h"
+#include "../shared/ums_request.h"
+#include <stddef.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(void) {
-  return 0;
+  int err = 0;
+  int fd = open("/dev/usermodscheddev", 0);
+
+  err = ioctl(fd, UMS_REQUEST_ENTER_UMS_SCHEDULING, 11);
 }
