a13e7b0a1f4205f0b95da0a23b55d1c4e7e76c86
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-18 23:37:08 +0200

    Add code for registering scheduler
    still not tested, from the module side the implementation of entry
    point registration in missing

--- src/user/ums_api.c
+++ src/user/ums_api.c
@@ -1,27 +1,59 @@
+#define _GNU_SOURCE
 #include "ums_api.h"
+#include "../shared/ums_request.h"
 #include <stddef.h>
-#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sched.h>
 
+#define TASK_STACK_SIZE 65536
 
 static void* do_gen_ums_sched(void *args);
+/* TODO: Move to header */
+typedef int (*ums_function)(int);
 
-int EnterUmsSchedulingMode(struct ums_entry_point entry_point,
-                           struct ums_completion_list comp_list)
+struct sched_entry_point {
+	ums_sched_id id;
+	int	     fd;
+	ums_function entry_point;
+};
+
+
+static void register_entry_point(struct sched_entry_point sched_ep);
+
+static int __entry_point(void *sched_ep);
+
+int EnterUmsSchedulingMode(ums_function entry_point,
+                           ums_complist_id complist_id,
+			   ums_sched_id *result)
 {
-  pthread_t ums_pthread;
+	int err, buff, fd;
+	struct sched_entry_point sched_ep;
 
-  int err = 0;
+	buff = complist_id;
 
-  err = pthread_create(&ums_pthread, NULL, &do_gen_ums_sched, NULL);
+	err = ioctl(fd, UMS_REQUEST_ENTER_UMS_SCHEDULING, buff);
 
-  if (err) {
-    fprintf(stderr, "Error: cannot create User Mode Scheduler thread!\n");
-  }
+	if (err) {
+		fprintf(stderr, "Error: cannot create User Mode Scheduler thread!\n");
+	}
 
-  return err;
+	*result = buff;
+
+	sched_ep.fd = fd;
+	sched_ep.id = *result;
+	sched_ep.entry_point = entry_point;
+
+	register_entry_point(sched_ep);
+
+	// register_threads(*result);
+
+	return err;
 }
 
+#if 0
 void ExecuteUmsThread(struct ums_scheduler* scheduler,
                       struct ums_worker* new_worker)
 {
@@ -45,4 +77,37 @@ static void* do_gen_ums_sched(void *args)
   printf("Executing function: %s\n", __func__);
 
   return 0;
+}
+
+#endif
+
+static void register_entry_point(struct sched_entry_point sched_ep)
+{
+	int thread_pid;
+	void *stack;
+
+	stack = malloc(TASK_STACK_SIZE);
+
+	thread_pid = clone(__entry_point, stack + TASK_STACK_SIZE, 
+			   CLONE_VM | CLONE_THREAD, &sched_ep);
+}
+
+static int __entry_point(void *sched_ep)
+{
+	/* USA I MESSAGGI */
+	int res;
+
+	struct sched_entry_point *_sched_ep = (struct sched_entry_point*)sched_ep;
+	
+	res = ioctl(_sched_ep->fd, UMS_REQUEST_REGISTER_ENTRY_POINT, _sched_ep->id);
+
+	if (res)
+		return res;
+
+	/* ums_sched module should block the process here. */
+	/* The internal function will take the identifier as a parameter */
+	_sched_ep->entry_point(_sched_ep->id);
+
+	/* not reached */
+	return 0;
 }
--- src/user/ums_api.h
+++ src/user/ums_api.h
@@ -1,8 +1,7 @@
 #ifndef __UMS_LINUX_H__
 #define __UMS_LINUX_H__
 
-#include "../shared/ums_structs.h"
-
+#if 0
 int EnterUmsSchedulingMode(struct ums_entry_point entry_point,
                            struct ums_completion_list comp_list);
 
@@ -13,4 +12,5 @@ void UmSThreadYield(struct ums_scheduler* scheduler,
                     struct ums_worker* act_worker);
 
 struct ums_worker* DequeueUmsCompletionListItems(struct ums_scheduler* scheduler);
+#endif
 #endif /* __UMS_SCHED_LINUX_H__ */
