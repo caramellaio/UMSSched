4e3384f109d6f6c7eef32c4b0e50bb6e427800b3
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-19 10:18:05 +0200

    Adapt the header to the new implementation

--- src/user/ums_api.h
+++ src/user/ums_api.h
@@ -1,10 +1,24 @@
 #ifndef __UMS_LINUX_H__
 #define __UMS_LINUX_H__
 
-#if 0
-int EnterUmsSchedulingMode(struct ums_entry_point entry_point,
-                           struct ums_completion_list comp_list);
+#include "../module/ums_scheduler.h"
+#include "../module/ums_complist.h"
+typedef int (*ums_function)(int);
 
+int EnterUmsSchedulingMode(ums_function entry_point,
+                           ums_complist_id complist_id,
+			   ums_sched_id *result);
+
+int CreateEmptyUmsCompletionList(ums_complist_id *id);
+
+int CreateUmsCompletionList(ums_complist_id *id,
+			    ums_function *list,
+			    int list_count);
+
+int CreateUmsCompletionElement(ums_complist_id id,
+		               ums_function func);
+
+#if 0
 void ExecuteUmsThread(struct ums_scheduler* scheduler,
                       struct ums_worker* new_worker);
 
