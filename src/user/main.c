6169885f07562dbdfac8b9efd408c934dbbfbf1f
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-19 22:08:57 +0200

    Add example calls to ums API

--- src/user/main.c
+++ src/user/main.c
@@ -1,16 +1,65 @@
 #include "ums_api.h"
 #include "../shared/ums_request.h"
 #include <stddef.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
-#include <fcntl.h>
+
+static int c = 0;
+
+static int incr_c(int ums_sched);
+
+static int decr_c(int ums_sched);
+
+static int mult_c(int ums_sched);
+
+static int entry_point(int ums_sched);
 
 int main(void) {
-  int err = 0;
-  int fd = open("/dev/usermodscheddev", 0);
+	int err = 0;
+	ums_sched_id sched_id;
+	ums_complist_id complist_id;
 
-  err = ioctl(fd, UMS_REQUEST_ENTER_UMS_SCHEDULING, 11);
+	ums_function funcs[3] = {
+		incr_c,
+		decr_c,
+		mult_c
+	};
+
+	CreateUmsCompletionList(&complist_id, funcs, 3);
+	
+	EnterUmsSchedulingMode(entry_point, complist_id, &sched_id);
+
+	sleep(3);
+
+	return err;
+}
+
+static int incr_c(int ums_sched)
+{
+	for(;;)
+		c++;
+
+	return c;
+}
+
+static int decr_c(int ums_sched)
+{
+	for(;;)
+		c--;
+
+	return c;
+}
+
+static int mult_c(int ums_sched)
+{
+	for(;;)
+		c*=c;
+
+	return c;
+}
+
+static int entry_point(int ums_sched)
+{
+	return 0;
 }
