5a7d34cc99822f049f3395eb78401b395b819d59
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-08-28 11:07:41 +0200

    Add a proper user mode example

--- src/user/main.c
+++ src/user/main.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 static int c = 0;
+static int index = 0;
 
 static int incr_c(int ums_sched);
 
@@ -30,37 +31,66 @@ int main(void) {
 	
 	EnterUmsSchedulingMode(entry_point, complist_id, &sched_id);
 
-	WaitUmsChildren();
-
 	getchar();
+
+	fprintf(stderr, "Final value of c: %d\n", c);
+
 	return err;
 }
 
 static int incr_c(int ums_sched)
 {
-	for(;;)
-		c++;
+	fprintf(stderr, "I am completion element %d\n", ums_sched);
+	fprintf(stderr, "incrementing c\n");
+	c++;
+	sleep(1);
+	
+
+	/* It is not sched! */
+	UmsThreadYield(ums_sched);
 
 	return c;
 }
 
 static int decr_c(int ums_sched)
 {
-	for(;;)
-		c--;
+	fprintf(stderr, "I am completion element %d\n", ums_sched);
+	fprintf(stderr, "decrementing c\n");
+	c--;
+	sleep(1);
+
+	UmsThreadYield(ums_sched);
 
 	return c;
 }
 
 static int mult_c(int ums_sched)
 {
-	for(;;)
-		c*=c;
+	fprintf(stderr, "I am completion element %d\n", ums_sched);
+	fprintf(stderr, "multiplying c\n");
+	c*=c;
+	sleep(1);
+
+	UmsThreadYield(ums_sched);
 
 	return c;
 }
 
 static int entry_point(int ums_sched)
 {
-	return 0;
+	int res;
+	fprintf(stderr, "entry_point: \n");
+	index = index % 3;
+	index++;
+	fprintf(stderr, "Index: %d\n", index);
+	if (ExecuteUmsThread(ums_sched, index++)) {
+		fprintf(stderr, "exec failed!\n");
+		res = -1;
+	}
+
+	UmsThreadYield(ums_sched);
+
+	fprintf(stderr, "end entry_point: \n");
+	index = index % 3;
+	return res;
 }
