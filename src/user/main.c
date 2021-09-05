299f2367e54aee4c14dab68bfee017c42b57563b
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-09-05 18:25:16 +0200

    Update main example/test

--- src/user/main.c
+++ src/user/main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define SLEEP_TIME 10
 static int c = 0;
 static int index = 0;
 
@@ -21,11 +22,18 @@ int main(void) {
 	ums_sched_id sched_id;
 	ums_complist_id complist_id;
 
-	ums_function funcs[1] = {
-		mult_c
+	ums_function funcs[8] = {
+		mult_c,
+		mult_c,
+		incr_c,
+		decr_c,
+		mult_c,
+		mult_c,
+		incr_c,
+		decr_c
 	};
 
-	if (CreateUmsCompletionList(&complist_id, funcs, 1)) {
+	if (CreateUmsCompletionList(&complist_id, funcs, 8)) {
 		fprintf(stderr, "Fail creating complist\n");
 		return -1;
 	}
@@ -43,7 +51,7 @@ static int incr_c(int ums_sched)
 	fprintf(stderr, "I am completion element %d\n", ums_sched);
 	fprintf(stderr, "incrementing c\n");
 	c++;
-	sleep(1);
+	sleep(SLEEP_TIME);
 	
 
 	return c;
@@ -54,7 +62,7 @@ static int decr_c(int ums_sched)
 	fprintf(stderr, "I am completion element %d\n", ums_sched);
 	fprintf(stderr, "decrementing c\n");
 	c--;
-	sleep(1);
+	sleep(SLEEP_TIME);
 
 	return c;
 }
@@ -64,7 +72,7 @@ static int mult_c(int ums_sched)
 	fprintf(stderr, "I am completion element %d\n", ums_sched);
 	fprintf(stderr, "multiplying c\n");
 	c*=c;
-	sleep(1);
+	sleep(SLEEP_TIME);
 
 	return c;
 }
@@ -75,27 +83,26 @@ static int entry_point(int ums_sched)
 	int res_len;
 	int shared[2];
 
-	fprintf(stderr, "entry_point: \n");
+	while (1) {
+		fprintf(stderr, "entry_point: \n");
 
-	res = UmsThreadYield();
-	fprintf(stderr, "entry_point2: \n");
+		DequeueUmsCompletionListItems(1, shared, &res_len);
 
-	DequeueUmsCompletionListItems(1, shared, &res_len);
+		if (res_len <= 0) {
+			fprintf(stderr, "dequeue failed!!!\n");
+			return -1;
+		}
 
-	if (res_len <= 0) {
-		fprintf(stderr, "dequeue failed!!!\n");
-		return -1;
+		fprintf(stderr, "compelem to exec: %d\n", shared[0]);
+
+		if (ExecuteUmsThread(shared[0])) {
+			fprintf(stderr, "exec failed!\n");
+			res = -1;
+		}
+
+		fprintf(stderr, "end entry_point: \n");
 	}
 
-	fprintf(stderr, "compelem to exec: %d\n", shared[0]);
-
-	if (ExecuteUmsThread(shared[0])) {
-		fprintf(stderr, "exec failed!\n");
-		res = -1;
-	}
-
-	fprintf(stderr, "end entry_point: \n");
-	index = index % 3;
-
+	fprintf(stderr, "THE IMPOSSIBLE HAPPENED!\n");
 	return res;
 }
