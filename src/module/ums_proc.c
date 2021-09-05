c4b1c5646ecc6f534f2443de1926e1dc33e08235
abombardelli <bombardelli.1903114@studenti.uniroma1.it>
2021-09-05 09:49:19 +0200

    Complete ums_proc module
    It now compiles without multiple declaration errors + add new utility
    functions

---
+++ src/module/ums_proc.c
@@ -0,0 +1,16 @@
+#include "ums_proc.h"
+
+struct proc_dir_entry *ums_proc_dir;
+
+int ums_proc_init(void)
+{
+	ums_proc_dir = proc_mkdir(UMS_PROC_DIR_NAME, NULL);
+
+	return ! ums_proc_dir;
+}
+
+void ums_proc_deinit(void)
+{
+	proc_remove(ums_proc_dir);
+	ums_proc_dir = NULL;
+}
--- src/module/ums_proc.h
+++ src/module/ums_proc.h
@@ -1,19 +1,37 @@
 #ifndef __UMS_PROC_H__
 #define __UMS_PROC_H__
+
 #include <linux/proc_fs.h>
-struct proc_dir_entry *ums_proc_dir;
+
+extern struct proc_dir_entry *ums_proc_dir;
+
+int ums_proc_init(void);
+
+void ums_proc_deinit(void);
 
 #define UMS_PROC_DIR_NAME "ums"
 
-#define ums_proc_init()							\
-	(NULL == (ums_proc_dir = proc_mkdir(UMS_PROC_DIR_NAME, NULL)))
-
-#define ums_proc_deinit()						\
-	do {								\
-		proc_remove(ums_proc_dir);				\
-		ums_proc_dir = NULL;					\
-	} while (0)
+#define NAME_BUFF 128
+#define UMS_FILE_MODE 0444
 
 #define ums_proc_root() (ums_proc_dir)
+
+#define ums_proc_geniddir(id, parent, res)				\
+	do {								\
+		char __proc_iddir_name[NAME_BUFF];			\
+		sprintf(__proc_iddir_name, "%d", id);			\
+		*(res) = proc_mkdir(__proc_iddir_name, parent);		\
+	} while (0)
+
+#define ums_proc_genidfile(id, parent, proc_ops, data, res)		\
+	do {								\
+		char __proc_idfile_name[NAME_BUFF];			\
+		sprintf(__proc_idfile_name, "%d", id);			\
+		*(res) = proc_create_data(__proc_idfile_name,		\
+					UMS_FILE_MODE, parent,		\
+					proc_ops, data);		\
+	} while (0)
+
+#define ums_proc_delete(proc) (proc_remove(proc))
 
 #endif /* __UMS_PROC_H__ */
